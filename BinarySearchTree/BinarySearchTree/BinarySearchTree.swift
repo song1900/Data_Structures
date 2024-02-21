990df8a3ade024b0703bd0aa6bcdd8831c7e85cf
song1900 <woojin1900@gmail.com>
2024-02-21 18:41:44 +0900

    ✨Feat: BST remove

--- BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
+++ BinarySearchTree/BinarySearchTree/BinarySearchTree.swift
@@ -37,7 +37,7 @@ class BinarySearchTree<T: Comparable> {
             
     }
     
-    public func search(value:T) -> Bool {
+    func search(value:T) -> Bool {
         var node: Node? = root
         while node != nil {
             if node!.data == value {
@@ -47,6 +47,74 @@ class BinarySearchTree<T: Comparable> {
         }
         return false
     }
+    
+    func remove(value: T) {
+        guard root == nil else {
+            return
+        }
+        
+        var parent: Node = root!
+        var node: Node? = root
+        
+        while node != nil {
+            if node!.data == value {
+                
+                if node?.left != nil && node?.right != nil { // 자식이 모두 존재
+                    let rightChild = node?.right
+                    node = findLeftChildMaxValue(node: node!.left!)
+                    node?.right = rightChild
+                    
+                } else if node?.left != nil { // 자식이 왼쪽만 존재
+                    if node!.data < parent.data {
+                        parent.left = node?.left
+                    } else {
+                        parent.right = node?.left
+                    }
+                    
+                } else if node?.right != nil { // 자식이 오른쪽만 존재
+                    if node!.data < parent.data {
+                        parent.left = node?.right
+                    } else {
+                        parent.right = node?.right
+                    }
+                } else { // 자식이 없음
+                    if node?.data == root?.data {
+                        root = nil
+                        return
+                    }
+                    if node!.data < parent.data {
+                        parent.left = nil
+                    } else {
+                        parent.right = nil
+                    }
+                    
+                }
+                
+                if value == root?.data {
+                    self.root = node!
+                }
+                break
+            }
+            
+            parent = node!
+            node = node!.data > value ? node?.left : node?.right
+        }
+    }
+    
+    private func findLeftChildMaxValue(node:Node<T>) -> Node<T> {
+        var parent:Node<T> = node
+        var rightChild:Node? = node.right
+        
+        while rightChild?.left != nil {
+            rightChild = rightChild?.right!
+            parent = rightChild!
+        }
+        if rightChild?.left != nil {
+            parent.left = rightChild?.left
+        }
+        return rightChild ?? parent
+    }
+    
     
     func printChild(node: Node<Int>?) {
         if node == nil { return }
--- BinarySearchTree/BinarySearchTree/main.swift
+++ BinarySearchTree/BinarySearchTree/main.swift
@@ -16,7 +16,11 @@ bst.insert(value: 1)
 bst.insert(value: 3)
 bst.printChild(node: bst.root)
 
-print(bst.search(value: 11)) //true
-bst.search(value: 100) //false
-bst.search(value: 0) //false
-bst.search(value: 8) //true
+//print(bst.search(value: 11)) //true
+//bst.search(value: 100) //false
+//bst.search(value: 0) //false
+//bst.search(value: 8) //true
+
+print("=================")
+bst.remove(value: 3)
+bst.printChild(node: bst.root)
